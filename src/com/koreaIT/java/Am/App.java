49fb1a036ef40a8aee22837b364ba43ebae750ea
ohjaesik <ojaesig86@gmail.com>
2022-02-21 12:45:03 +0900

    특정 게시물을 찾는 메서드 구현, 관련 중복 제거

--- src/com/koreaIT/java/Am/App.java
+++ src/com/koreaIT/java/Am/App.java
@@ -50,15 +50,8 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
+				
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
@@ -75,16 +68,7 @@ public class App {
 			} else if (command.startsWith("article delete ")) {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
-				int foundIndex = -1;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-
-						foundIndex = i;
-						break;
-					}
-				}
+				int foundIndex = getArticleIndexById(id);
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -98,15 +82,9 @@ public class App {
 				String[] commandBits = command.split(" ");
 				int id = Integer.parseInt(commandBits[2]);
 
-				Article foundArticle = null;
+				Article foundArticle = getArticleById(id);
 
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
@@ -142,6 +120,29 @@ public class App {
 
 		System.out.println("== 프로그램 끝 ==");
 
+	}
+
+	private int getArticleIndexById(int id) {
+		int i = 0;
+		for (Article article : articles) {
+
+			if (article.id == id) {
+				 return i;
+			}
+			i++;
+		}
+
+		return -1;
+	}
+
+	private Article getArticleById(int id) {
+		int index = getArticleIndexById(id);
+		if (index != -1) {
+			return articles.get(index);
+		}
+		
+
+		return null;
 	}
 
 	public void makeTestData() {
